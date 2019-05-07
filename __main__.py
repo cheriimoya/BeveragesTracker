e63a5e4cf9baf1b702c441886b7746b5364f56e2
Max Hausch <cheriimoya@googlemail.com>
2019-05-07 16:42:27 +0200

    Put code into new class

--- __main__.py
+++ __main__.py
@@ -1,50 +1,7 @@
-import json
-
+from beverages_tracker import BeveragesTracker
 def main():
-    running = True
-
-    entries = load_data()
-
-    try:
-        while running:
-            id = read_id()
-            update_amount_for_id(entries, id)
-            print('Update: ' + str(entries))
-            save_data(entries)
-
-    except KeyboardInterrupt:
-        pass
-    finally:
-        save_data(entries)
-
-def read_id():
-    id = None
-    while id is None:
-        try:
-            id = int(input('Please give me your Matrikelnummer: '))
-            break
-        except KeyboardInterrupt as abort:
-            raise abort
-        except:
-            pass
-    return str(id)
-
-def update_amount_for_id(entries, id):
-    if id not in entries:
-        entries[id] = 0
-
-    entries[id] += 1
-    return entries[id]
-
-def save_data(entries):
-    with open('entries.json', 'w') as save_file:
-        save_file.write(json.dumps(entries))
-
-def load_data():
-    jentries = {}
-    with open('entries.json') as entries:
-        jentries = json.loads(entries.read())
-    return jentries
+    bev = BeveragesTracker()
+    bev.start_loop()
 
 if __name__ == '__main__':
     main()
---
+++ beverages_tracker.py
@@ -0,0 +1,49 @@
+import json
+
+
+class BeveragesTracker:
+
+    def __init__(self):# {{{
+        self.running = True
+        self.entries = self.load_data()# }}}
+
+    def start_loop(self):# {{{
+        try:
+            while self.running:
+                id = self.read_id()
+                self.update_amount_for_id(id)
+                print('Update: ' + str(self.entries))
+                self.save_data()
+
+        except KeyboardInterrupt:
+            pass
+        finally:
+            self.save_data()# }}}
+
+    def read_id(self):# {{{
+        id = None
+        while id is None:
+            try:
+                id = int(input('Please give me your Matrikelnummer: '))
+                break
+            except KeyboardInterrupt as abort:
+                raise abort
+            except:
+                pass
+        return str(id)# }}}
+
+    def update_amount_for_id(self, id):# {{{
+        if id not in self.entries:
+            self.entries[id] = 0
+        self.entries[id] += 1# }}}
+
+    def save_data(self):# {{{
+        with open('entries.json', 'w') as save_file:
+            save_file.write(json.dumps(self.entries))# }}}
+
+    def load_data(self):# {{{
+        try:
+            with open('entries.json') as entries:
+                return json.loads(entries.read())
+        except:
+            return json.loads('{}')# }}}
