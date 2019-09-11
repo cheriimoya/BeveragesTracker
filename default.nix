eac404cad5fb557423a66b13e251e570a010fd10
Max Hausch <cheriimoya@googlemail.com>
2019-09-12 01:24:00 +0200

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import <nixpkgs> {  } }:
+
+let
+  python36-beverages-tracker = pkgs.python36.withPackages (p: with p; [
+    requests
+    tkinter
+  ]);
+in
+
+pkgs.stdenvNoCC.mkDerivation {
+  name = "python36-beverages-tracker";
+  nativeBuildInputs = with pkgs; [
+    python36-beverages-tracker
+  ];
+}
+
+
