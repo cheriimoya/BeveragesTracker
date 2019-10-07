ab0b235397033aef3196ea41fcb02fb6f2a75ce6
Max Hausch <cheriimoya@googlemail.com>
2019-10-07 18:17:09 +0200

    Add function to print consumed liters

--- beveragesplot/plotTotal.py
+++ beveragesplot/plotTotal.py
@@ -15,6 +15,45 @@ def autolabel(rects, ax):
                     )
 
 
+def plot_liters(entries):
+    labels = [obj.name for obj in entries]
+    drinks = [obj.drinks for obj in entries]
+
+    liters_total = []
+
+    for drink in drinks:
+        liter = 0
+        for d in drink:
+            liter += drink[d]
+        liters_total.append(liter)
+
+    x = np.arange(len(labels))  # the label locations
+    width = 0.35  # the width of the bars
+
+    plt.style.use('dark_background')
+
+    # disable toolbar
+    plt.rcParams['toolbar'] = 'None'
+
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x/2, liters_total, width)
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('in Euro', fontsize=20)
+    ax.set_xlabel('Fachschaftsmitglieder', fontsize=20)
+    ax.set_title('Schuldenberg der Fachschaftsmitglieder\n'
+                 'WO IST UNSER GELD ???', fontsize=20)
+    ax.set_xticks(x/2)
+    ax.set_xticklabels(labels, fontsize=20)
+    ax.legend()
+
+    autolabel(rects1, ax)
+
+    fig.tight_layout()
+
+    return plt
+
+
 def plotTotal(entries):
     labels = [obj.name for obj in entries]
     owes = [obj.owes_total for obj in entries]
