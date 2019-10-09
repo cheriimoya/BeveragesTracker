27aa53ad18b2f6bed189245f7c64f7106466152f
LePampelmuse <T_F_T@web.de>
2019-10-09 16:56:01 +0200

    added new function for ploting specific drinks, added some doku, changed function name plotTotal

--- beveragesplot/__main__.py
+++ beveragesplot/__main__.py
@@ -5,8 +5,8 @@ import plotTotal as pT
 import entries as et
 import urllib.request
 
-WEBSERVER_PATH = 'http://192.168.1.21:8000/'
-
+#WEBSERVER_PATH = 'http://192.168.1.21:8000/'
+WEBSERVER_PATH='http://www2.hs-esslingen.de/~mahait13/'
 
 def main():
     owe_list_old = []
@@ -22,14 +22,24 @@ def main():
         except:
             entries = []
             persons = []
-
+        
         id_list = et.from_json(entries, persons)
         owe_list = [obj.owes_total for obj in id_list]
 
+        # check after sleep if list is new
         if owe_list != owe_list_old and entries:
+            # plot graph for specific drinks
+            pT.plot_specific_drink(id_list, 'Oetti Export')
+            pT.plot_specific_drink(id_list, 'Kaffee')
+
+            # plot graph for liters 
             pT.plot_liters(id_list)
             pT.plot_liters_detailed(id_list)
-            pT.plotTotal(id_list)
+
+            # plot graph for total owes 
+            pT.plot_total_owe_list(id_list)
+            
+            # set new list to old list
             owe_list_old = owe_list
         else:
             sleep(5)
